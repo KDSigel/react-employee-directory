96250d91a4c915c67f10db28b6701131b8a2a626
KDSigel <heykarl@gmail.com>
2022-01-05 15:05:05 -0800

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+<Router>
+  <Switch>
+    <Route>
+
+    </Route>
+  </Switch>
+</Router>
+  );
 }
