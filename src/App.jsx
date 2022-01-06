65090b2a415be62fffae7aa280b4b5b7ec61599a
KDSigel <heykarl@gmail.com>
2022-01-06 15:42:50 -0800

    added usercontext and privateroute

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header/Header';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import { UserProvider } from './context/UserContext';
 import EditProfile from './views/EditProfile/EditProfile';
 import Home from './views/Home/Home';
 import LogIn from './views/LogIn.jsx/LogIn';
@@ -8,25 +10,27 @@ import Register from './views/Register/Register';
 
 export default function App() {
   return (
-<Router>
-  <Header />
-  <Switch>
-    <Route exact path='/'>
-      <Home />
-    </Route>
-    <Route path='/login'>
-      <LogIn />
-    </Route>
-    <Route path='/register'>
-      <Register />
-    </Route>
-    <Route path='/profile'>
-      <Profile />
-    </Route>
-    <Route path='/editprofile'>
-      <EditProfile />
-    </Route>
-  </Switch>
-</Router>
+    <UserProvider>
+      <Router>
+        <Header />
+        <Switch>
+          <PrivateRoute exact path="/">
+            <Home />
+          </PrivateRoute>
+          <Route path="/login">
+            <LogIn />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <PrivateRoute path="/profile">
+            <Profile />
+          </PrivateRoute>
+          <PrivateRoute path="/editprofile">
+            <EditProfile />
+          </PrivateRoute>
+        </Switch>
+      </Router>
+    </UserProvider>
   );
 }
---
+++ src/context/UserContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useContext, useState } from 'react';
+import { getUser } from '../services/users';
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const currentUser = getUser();
+  const [user, setUser] = useState(
+    currentUser ? { id: currentUser.id, email: currentUser.email } : null
+  );
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+const useUser = () => {
+    const userCtx = useContext(UserContext)
+    if (userCtx === undefined) {
+        throw new Error('useUser must be used within a UserProvider');
+    }
+    return userCtx;
+};
+
+export { UserProvider, useUser };
